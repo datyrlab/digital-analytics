5fe3ab6509a161eeec8df40040cf0cd63fd6acf6
datyrlab <datyrlab@gmail.com>
2023-04-24 18:13:31 +0100

    update

---
+++ adobe_python/tests/test_modules.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import json, os, platform, re, sys, unittest, time
+
+package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_dir = os.path.dirname(package_dir)
+sys.path.insert(0, project_dir)
+
+class TestOpencv(unittest.TestCase):
+
+    def test_platform(self):
+        print(platform.platform())
+
+    def test_env(self):
+        print(os.environ)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
