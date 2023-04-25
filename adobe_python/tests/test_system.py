175a3f1427b5b26db2bf462bdaee7bc496010323
datyrlab <datyrlab@gmail.com>
2023-04-25 11:26:45 +0100

    update

--- adobe_python/tests/test_system.py
+++ adobe_python/tests/test_system.py
@@ -6,17 +6,14 @@ package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 project_dir = os.path.dirname(package_dir)
 sys.path.insert(0, project_dir)
 
-class TestModules(unittest.TestCase):
+class TestSystem(unittest.TestCase):
 
     def test_platform(self):
         print(platform.platform())
 
     def test_env(self):
         print(os.environ)
-
-    def test_env_adobe(self):
-        print("ADOBE_CONFIG", os.environ['ADOBE_CONFIG'])
-
+        print("ADOBE_CONFIG:", os.environ['ADOBE_CONFIG'])
 
 
 if __name__ == '__main__':
