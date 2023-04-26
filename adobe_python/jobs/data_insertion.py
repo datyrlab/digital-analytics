93aad06360e0239e49f0c98524f576aba3c1a4f3
datyrlab <datyrlab@gmail.com>
2023-04-26 16:20:33 +0100

    update

--- adobe_python/jobs/data_insertion.py
+++ adobe_python/jobs/data_insertion.py
@@ -77,7 +77,7 @@ def sendCommand(index:int, request:dict, filepath:str) -> None:
     if isinstance(r, dict):
         run = class_subprocess.Subprocess({}).run(r.get('command'))
         
-        print(f"\033[0;37;45m{data}\033[0m")
+        print(f"\033[0;37;45m{r.get('data')}\033[0m")
         print(f"\033[1;30;47m{run}\033[0m")
 
         #if re.search("SUCCESS", run):
