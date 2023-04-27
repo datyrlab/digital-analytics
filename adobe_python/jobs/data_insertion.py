854937aca0ef49680052e0de3dc51f3baaea38a4
datyrlab <datyrlab@gmail.com>
2023-04-27 11:29:27 +0100

    update

--- adobe_python/jobs/data_insertion.py
+++ adobe_python/jobs/data_insertion.py
@@ -62,14 +62,14 @@ def getCommand(url:str, streamid:str, filepath:str) -> dict:
             data["event"]["xdm"]["timestamp"] = tsformat 
         logfile = f"{dir_log}/{tsinteger}.json"
         #d = json.dumps(data).replace('"', '\\"')
-        d = json.dumps(data)
+        d = json.dumps(data).replace('"', '\\"\\"') if re.search("^Windows", myplatform) else json.dumps(data)
         s = []
         s.append(f"curl -X POST \"https://server.adobedc.net/ee/v2/interact?dataStreamId={streamid}\"")
         s.append(f"-H \"Authorization: Bearer {t.get('token')}\"")
         s.append(f"-H \"x-gw-ims-org-id: {t.get('orgid')}\"")
         s.append(f"-H \"x-api-key: {t.get('apikey')}\"")
         s.append(f"-H \"Content-Type: application/json\"")
-        s.append(f"-d '{json.dumps(data)}'") if re.search("^Linux", myplatform) else s.append(f"-d \"{json.dumps(data)}\"")
+        s.append(f"-d \"{d}\"") if re.search("^Windows", myplatform) else s.append(f"-d '{d}'")
         command = " ".join(s)
         return {"logfile":logfile, "data":data, "command":command}
 
