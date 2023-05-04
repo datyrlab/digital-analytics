7dd0dd3e9bbd3c606434204bff610914c16aa26d
datyrlab <datyrlab@gmail.com>
2023-05-04 11:36:00 +0100

    update

--- adobe_python/jobs/data_insertion.py
+++ adobe_python/jobs/data_insertion.py
@@ -120,7 +120,7 @@ def sendCommand(index:int, request:dict, filepath:str) -> None:
                 class_files.Files({}).writeFile({"file":f"{directory_response}/{response.get('requestId')}_{r.get('time')}.json", "content":json.dumps(response, sort_keys=False, default=str)}) if re.search("^requestId", run) else None 
                 class_files.Files({}).writeFile({"file":f"{directory_log}/{r.get('time')}-log.json", "content":json.dumps({"request":r.get('data'), "response":response}, sort_keys=False, default=str)})  
                 class_files.Files({}).writeFile({"file":f"{directory_log}/{r.get('time')}-format.json", "content":json.dumps({"request":r.get('data'), "response":response}, sort_keys=False, indent=4, default=str)})  
-                print("requestId:", response.get('requestId'))
+                print("requestId:", response.get('requestId')) if isinstance(response, dict) else print("error:", run)
             except Exception as e:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e) 
 
