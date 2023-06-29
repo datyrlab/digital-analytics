5ae3f4023ef0848012d043daec5f22b9212ac228
datyrlab <datyrlab@gmail.com>
2023-06-29 20:55:22 +0200

    update

--- adobe_python/jobs/stream_aep.py
+++ adobe_python/jobs/stream_aep.py
@@ -9,9 +9,10 @@ package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 project_dir = os.path.dirname(package_dir)
 sys.path.insert(0, project_dir)
 from adobe_python.classes import class_converttime, class_files, class_subprocess
-from adobe_python.jobs import oauth
+from adobe_python.jobs import id_service, oauth
 
 timestamp_numeric = int(time.time() * 1000.0)
+dir_device_rel = f"myfolder/device"
 dir_tmp = f"{project_dir}/myfolder/events-sent/tmp"
 dir_log = f"{project_dir}/myfolder/events-sent/logs"
 dir_json = f"{package_dir}/json"
@@ -27,7 +28,6 @@ def parseArgs(argv) -> tuple:
     parser = argparse.ArgumentParser()
     parser.add_argument('-re', '--request', dest='request')
     namespace = parser.parse_known_args(argv)[0]
-    
     args = {k: v for k, v in vars(namespace).items() if v is not None}
     request = json.loads(args.get('request')) if isinstance(args.get('request'), str) else None
     return request
@@ -35,8 +35,15 @@ def parseArgs(argv) -> tuple:
 def parseRequest(request:dict) -> None:
     if isinstance(request, dict):
         token = {} if dev else oauth.getAccessToken()
+        request.update({"identityMap":fpidNew()}) if not isinstance(request.get('identityMap'), str) else None
         eventlist = getEventList(request.get('eventlist'))
         [(lambda x: makeRequest(i, eventlist, token, request, f"{project_dir}/{x}"))(x) for i, x in enumerate(eventlist)] if isinstance(eventlist, list) and len(eventlist) > 0 else print("Event list is empty")
+
+def fpidNew() -> dict:
+    i = str(uuid.uuid4())
+    directory = f"{project_dir}/{dir_device_rel}/{i}"
+    makeDirectory(directory)
+    return f"{dir_device_rel}/{i}"
 
 def getEventList(eventlist:list) -> list:
     if isinstance(eventlist, list):
